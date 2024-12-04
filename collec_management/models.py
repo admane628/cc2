cfca74206ca71d98ca02692e16d2c0681f0c932b
admane628 <admane628@gmail.com>
2024-12-04 15:54:07 +0100

    Add files via upload

--- collec_management/models.py
+++ collec_management/models.py
@@ -1,14 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Collec(models.Model ) :
     title = models.CharField ( max_length =100 , null=False)
     description = models.TextField (null=False)
     date = models.DateTimeField (auto_now_add=True , null=False)
+    created_by=models.ForeignKey(User, on_delete=models.CASCADE )
 
     def __str__ ( self ) :
         return self.title
-    
+
 class Element(models.Model):
     title = models.CharField ( max_length =100 , null=False)
     description = models.TextField (null=False)
@@ -16,6 +18,7 @@ class Element(models.Model):
     value=models.IntegerField(null=False)
     quantity=models.IntegerField(null=False)
     collection=models.ForeignKey(Collec, on_delete=models.CASCADE )
+    created_by=models.ForeignKey(User, on_delete=models.CASCADE )
 
     def __str__ ( self ) :
         return f"{self.collection.title} - {self.title}"
--- collec_management/views.py
+++ collec_management/views.py
@@ -1,23 +1,26 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
-
+from django.contrib.auth.decorators import login_required
 from collec_management.forms import CollecForm, ElmForm
 from collec_management.models import Collec, Element
 # Create your views here.
 def about(request):
     return render(request, 'collec_management/about.html')
 
+@login_required
 def collection(request ,id) :
-    collectionInexistant= get_object_or_404(Collec , id=id)
+    collectionInexistant= get_object_or_404(Collec , id=id, created_by=request.user)
     elements = Element.objects.filter(collection=collectionInexistant)
     return render(request ,'collec_management/collection_detail.html', {'collectionInexistant' : collectionInexistant, "elements":elements})
 
+@login_required
 def new( request ) :
  if request.method == "POST" :
     form=CollecForm(request.POST)
     if form.is_valid () :
        collec=form.save(commit = False )
+       collec.created_by = request.user
        collec.save () # Sauvegarde en base de donnees
        return HttpResponseRedirect(reverse('collection' ,args=[collec.id] ))
  else :
@@ -29,8 +32,9 @@ def all(request):
     collections=Collec.objects.all()
     return render(request, "collec_management/colleclist.html", {"collections":collections})
 
+@login_required
 def delete(request , collec_id) :
-   collec=get_object_or_404(Collec , pk=collec_id)
+   collec=get_object_or_404(Collec , pk=collec_id, created_by=request.user)
    if request.method=="POST" :
       collec.delete()
       return redirect("all")
@@ -44,8 +48,9 @@ def check_save(form) :
       collec.save ()
    return collec.id
 
+@login_required
 def change(request , collec_id) :
-   collec = get_object_or_404(Collec , pk=collec_id)
+   collec = get_object_or_404(Collec , pk=collec_id, created_by=request.user)
    if request.method == "POST" :
       form =CollecForm(request.POST , instance=collec)
       collecModif_id = check_save(form)
@@ -58,12 +63,14 @@ def change(request , collec_id) :
 def menu(request):
     return render(request, 'collec_management/menu.html')
 
+@login_required
 def element(request ,id) :
-    elm = get_object_or_404(Element , id=id)
+    elm = get_object_or_404(Element , id=id, created_by=request.user)
     return render(request ,'collec_management/element_detail.html', {'elm' : elm})
 
+@login_required
 def element_edit(request, id):
-    elm = get_object_or_404(Element , pk=id)
+    elm = get_object_or_404(Element , pk=id, created_by=request.user)
     if request.method == "POST" :
        form =ElmForm(request.POST , instance=elm)
        elmModif_id = check_save(form)
